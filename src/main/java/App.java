dbcf12c0ab9065fc8e513bed1c43638540fff400
Al Aganya <tbone.aa87@gmail.com>
2017-04-21 17:27:11 +0300

    Added some random words Increased the difficulty by adding some longer harder words

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,7 +8,7 @@ public class App {
     List<Character> guess = new ArrayList<>();
     Random randomNumber  = new Random();
     String[] words =
-    { "cadillac", "mozilla", "player", "noun", "leafy", "surbubs","wineglass", "bottletop", "letters", "telephone", "kitchen", "happiness", "lacrosse", "america", "africa", "loving", "request", "mother", "daylight", "king"
+    { "cadillac", "mozilla", "surbubs","wineglass", "bottletop", "letters", "telephone", "kitchen", "happiness", "lacrosse", "america", "africa", "loving", "request", "mother", "daylight", "king"
     };
     Prompter prompter = new Prompter(new Hangman(words[randomNumber.nextInt(words.length)]));
 	// Prompter prompter = new Prompter(new Hangman("collins"));
